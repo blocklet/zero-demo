64a71849803dfe956049295f638e3fbf25bed9b2
wangshijun <wangshijun2010@gmail.com>
2020-11-25 16:15:07 +0800

    chore: polish the code layout for blocklet

---
+++ app/random.js
@@ -0,0 +1,4 @@
+// time.js
+module.exports = (req, res) => {
+  res.send({ random: Math.random() });
+};
--- time.js
+++
@@ -1,6 +0,0 @@
-// time.js
-const moment = require("moment")
-module.exports = (req, res) => {
-  var time = moment().format('LT');   // 11:51 AM
-  res.send({time: time })
-}
